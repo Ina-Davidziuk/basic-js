d436a8716d178213cbcff5a9f07cdf420261f58d
Ina-Davidziuk <ina.davidziuk@gmail.com>
2023-04-16 18:15:31 +0200

    complete task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,16 +19,97 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
+const cipher = ['A','B','C','D','E','F','G','H','I','J','K', 'L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(mode = true) {
+   this.mode = mode;
+ }
+
+ encrypt(message, key) {
+  if(!message || !key){
+  throw new Error ('Incorrect arguments!')
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  let spaceIndex =[];
+   for(let j = 0; j< message.length; j++){
+     if(message[j] === ' '){
+       spaceIndex.push([j])
+     }
+   }
+    message = message.toUpperCase().split(' ').join('');
+  key = key.toUpperCase();
+   
+   //str code
+   let result = ''
+   let textCode =[];
+   let keyCode =[];
+  let j=0;
+   for(let i =0; i<message.length; i++){
+   if(!cipher.includes(message[i])){
+     result +=message[i]
+     continue;
+   }
+let letterCode = cipher.indexOf(message[i]);
+let keyLetter = cipher.indexOf(key[j%key.length])
+
+result += cipher[(letterCode + keyLetter) % 26]
+  j++
   }
+  result = result.split('')
+  spaceIndex.forEach(function(el){
+ result.splice(el,0,' ')
+ })
+  return this.mode ? result.join('') : result.reverse().join('')
+ }
+ 
+ 
+ decrypt(message, key) {
+  if(!message || !key){
+  throw new Error ('Incorrect arguments!')
+  } 
+  let spaceIndex =[];
+  for(let j = 0; j< message.length; j++){
+  if(message[j] === ' '){
+  spaceIndex.push([j])
+     }
+   }
+   
+  
+   message = message.toUpperCase().split(' ').join('');
+   key = key.toUpperCase();
+   
+   let result = ''
+   let textCode =[];
+   let keyCode =[];
+  let j=0;
+   for(let i =0; i<message.length; i++){
+   if(!cipher.includes(message[i])){
+      result +=message[i]
+     continue
+   }
+   { 
+let letterCode = cipher.indexOf(message[i]);
+let keyLetter = cipher.indexOf(key[j%key.length])
+let index = (letterCode - keyLetter) % 26
+ if(index < 0){
+   index = index +26
+ }
+ result += cipher[index]
+  j++
+  }
+  
+   }
+  result = result.split('')
+  spaceIndex.forEach(function(el){
+ result.splice(el,0,' ')
+ })
+  return this.mode ? result.join('') : result.reverse().join('')
+ }
+
 }
+const directMachine = new VigenereCipheringMachine();
+
+const reverseMachine = new VigenereCipheringMachine(false);
 
 module.exports = {
   VigenereCipheringMachine
